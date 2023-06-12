ecbab8cf412b065fc8a80a8a050417f5529da662
re11n <97107417+re11n@users.noreply.github.com>
2023-06-12 12:55:14 +0700

    membuat form register

---
+++ Registration/RegistrationClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Registration
+{
+    public class RegistrationClass
+    {
+        public RegistrationClass()
+        {
+
+        }
+        public static bool checkUsername(string username)
+        {
+            var initialJson = File.ReadAllText("user.json");
+            dynamic data = JArray.Parse(initialJson);
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i].username == username)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool checkPassword(string password)
+        {
+            if (password.Length < 8)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static void createAkun(string name, string username, string password)
+        {
+            var initialJson = File.ReadAllText("user.json");
+            var array = JArray.Parse(initialJson);
+            var itemToAdd = new JObject();
+            itemToAdd["name"] = name;
+            itemToAdd["username"] = username;
+            itemToAdd["password"] = password;
+            array.Add(itemToAdd);
+
+            var jsonToOutput = JsonConvert.SerializeObject(array, Formatting.Indented);
+            File.WriteAllText("user.json", jsonToOutput);
+        }
+    }
+}
---
+++ RegistrationForm/RegForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Registration;
+
+namespace RegistrationForm
+{
+    public partial class RegForm : Form
+    {
+        public RegForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (RegistrationClass.checkUsername(namaTextBox.Text)){
+                MessageBox.Show("Username sudah ada, silahkan masukkan ulang.");
+            }else if (!RegistrationClass.checkPassword(pwTextBox.Text)) {
+                MessageBox.Show("Password minimal 8 karakter");
+            } else if (pwTextBox.Text != cpwTextBox.Text)
+            {
+                MessageBox.Show("password tidak sama, silahkan masukkan ulang");
+            }
+            else
+            { 
+                RegistrationClass.createAkun(namaTextBox.Text, usernameTextBox.Text, pwTextBox.Text);
+                MessageBox.Show("Akun Berhasil Dibuat");
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
